9ef37065637cb92d73793669f466b4fcd1bc73ee
Nick Stebb <nickolarse@riseup.net>
2022-07-05 17:07:05 +1200

    update habit creation

--- dna/zomes/atomic_habits/src/habit/entry.rs
+++ dna/zomes/atomic_habits/src/habit/entry.rs
@@ -1,14 +1,19 @@
+use std::collections::BTreeMap;
+
 use hdk::prelude::*;
-
-
-
-
 
 #[hdk_entry(id = "habit")]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone)]
 pub struct Habit {
-  pub name: String,
-  pub timeframe: String,
-  pub habit_metadata: String,
+    pub name: String,
+    pub timeframe: Timeframe,
+    pub metadata: BTreeMap<String, String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Timeframe {
+    pub start_time: String,
+    pub end_time: String,
 }
