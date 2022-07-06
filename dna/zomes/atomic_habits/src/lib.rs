4e1160afc92096c5bcc93a978bf26e2fe7c4432e
Nick Stebb <nickolarse@riseup.net>
2022-07-06 12:01:17 +1200

    add path entrydef

--- dna/zomes/atomic_habits/src/lib.rs
+++ dna/zomes/atomic_habits/src/lib.rs
@@ -4,11 +4,9 @@ mod habit;
 
 use habit::Habit;
 
-entry_defs![
-  Habit::entry_def()
-];
+entry_defs![Habit::entry_def(), PathEntry::entry_def()];
 
 #[hdk_extern]
 pub fn validate(_op: Op) -> ExternResult<ValidateCallbackResult> {
-  Ok(ValidateCallbackResult::Valid)
+    Ok(ValidateCallbackResult::Valid)
 }
