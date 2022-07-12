6cbd1e2fb94122874204f8ada10a59fc41af17c9
Nick Stebb <nickolarse@riseup.net>
2022-07-12 13:51:45 +1200

    reorganise crud

--- dna/zomes/atomic_habits/src/habit/handlers.rs
+++ dna/zomes/atomic_habits/src/habit/handlers.rs
@@ -15,38 +15,6 @@ impl From<HabitLinkType> for LinkType {
 }
 
 #[hdk_extern]
-pub fn get_habit(entry_hash: EntryHashB64) -> ExternResult<Option<Habit>> {
-    let maybe_element = get(EntryHash::from(entry_hash), GetOptions::default())?;
-
-    match maybe_element {
-        None => Ok(None),
-        Some(element) => {
-            let habit: Habit = element.entry().to_app_option()?.ok_or(WasmError::Guest(
-                "Could not deserialize element to Habit.".into(),
-            ))?;
-
-            Ok(Some(habit))
-        }
-    }
-}
-
-#[hdk_extern]
-pub fn get_all_habits(_: ()) -> ExternResult<Connection> {
-    let path = habits_path();
-    let children = get_links(path.path_entry_hash()?, None)?;
-
-    let habits: Vec<Habit> = children
-        .into_iter()
-        .map(|link| get_habit_from_link_target(link.target.into()))
-        .collect::<ExternResult<Vec<Habit>>>()?
-        .into_iter()
-        .collect();
-    let habit_connection = Connection::new(habits);
-
-    Ok(habit_connection)
-}
-
-#[hdk_extern]
 pub fn create_habit(habit: Habit) -> ExternResult<GraphQLCreatePayload> {
     let header_hash = create_entry(&habit.clone())?;
     let entry_hash = hash_entry(&habit)?;
@@ -77,6 +45,38 @@ pub fn create_habit(habit: Habit) -> ExternResult<GraphQLCreatePayload> {
     };
 
     Ok(output)
+}
+
+#[hdk_extern]
+pub fn get_habit(entry_hash: EntryHashB64) -> ExternResult<Option<Habit>> {
+    let maybe_element = get(EntryHash::from(entry_hash), GetOptions::default())?;
+
+    match maybe_element {
+        None => Ok(None),
+        Some(element) => {
+            let habit: Habit = element.entry().to_app_option()?.ok_or(WasmError::Guest(
+                "Could not deserialize element to Habit.".into(),
+            ))?;
+
+            Ok(Some(habit))
+        }
+    }
+}
+
+#[hdk_extern]
+pub fn get_all_habits(_: ()) -> ExternResult<Connection> {
+    let path = habits_path();
+    let children = get_links(path.path_entry_hash()?, None)?;
+
+    let habits: Vec<Habit> = children
+        .into_iter()
+        .map(|link| get_habit_from_link_target(link.target.into()))
+        .collect::<ExternResult<Vec<Habit>>>()?
+        .into_iter()
+        .collect();
+    let habit_connection = Connection::new(habits);
+
+    Ok(habit_connection)
 }
 
 #[hdk_extern]
@@ -144,7 +144,6 @@ fn get_habit_from_link_target(target_hash: EntryHash) -> ExternResult<Habit> {
 
 /** GraphQL Structs */
 #[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
 pub struct GraphQLCreatePayload {
     payload: NewHabitOutput,
 }
@@ -162,7 +161,7 @@ pub struct UpdateHabitInput {
     updated_habit: Habit,
 }
 
-#[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Connection {
     edges: Vec<Edge>,
     page_info: String,
@@ -177,7 +176,7 @@ impl Connection {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Edge {
     cursor: String,
     node: Habit,
